791534088dab92f0664781ffde330b94a188a5d8
Alex Leistra <alleistra@gmail.com>
2025-01-12 19:19:31 -0500

    layout edits

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -4,31 +4,24 @@ function About() {
       <div id="about" className="mb-4">
         <div className="">
           <div className="mb-4">
-            <h2>About</h2>
+            <h2>Local Tack Repair and Leather Work</h2>
           </div>
-          <div>
-            <div>
-              <h3>
+          <div className="mb-4">
+            <ul>
+              <li>
                 Repairs to Horse Harnesses, Bridles, Halters, Saddles and more
-              </h3>
-            </div>
-            <div>
-              <ul>
-                <li>
-                  Leather halters, British/Canadian Cavalry style bridles made
-                  on site.
-                </li>
-                <li>Leather belts and sheaths made to your specifications.</li>
-                <li>
-                  Premium quality replacement horse blanket leg straps in stock.
-                </li>
-                <li></li>
-                <li></li>
-              </ul>
-            </div>
+              </li>
+              <li>
+                Leather halters, British/Canadian Cavalry style bridles made on
+                site.
+              </li>
+              <li>Leather belts and sheaths made to your specifications.</li>
+              <li>
+                Premium quality replacement horse blanket leg straps in stock.
+              </li>
+            </ul>
           </div>
           <div>
-            <h3>Accepted Payments</h3>
             <p>
               Payment cash or E-Transfer. No Debit or Credit Cards accepted.
             </p>
--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -3,7 +3,7 @@ function Contact() {
     <>
       <section>
         <div>
-          <h2>Contact</h2>
+          <h2>Located in Mt. Brydges Ontario</h2>
         </div>
         <div>
           <p>
@@ -11,6 +11,20 @@ function Contact() {
             Mondays, National/Provincial Holidays, Nov. 11th. John's Tack Repair
             Service
           </p>
+        </div>
+        <div>
+          <div className="relative flex flex-col my-6 bg-white shadow-sm border border-slate-200 rounded-lg w-96">
+            <div className="p-4">
+              <h5 className="mb-2 text-xl ">
+                Website Review Check Update from Our Team in San Francisco
+              </h5>
+              <p className="leading-normal font-light">
+                The place is close to Barceloneta Beach and bus stop just 2 min
+                by walk and near to Naviglio where you can enjoy the main night
+                life in Barcelona.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </>
