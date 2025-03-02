4b128be92640cd6c4bbe95bf37b13c3767b759dd
Alex Leistra <alleistra@gmail.com>
2025-03-02 13:23:26 -0500

    image dialog

--- src/components/About/Images.tsx
+++ src/components/About/Images.tsx
@@ -5,10 +5,26 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+
 import CarouselImage from "./CarouselImage";
+import { useState } from "react";
+import ImageProxy from "../ImageProxy";
 
 export default function Images() {
-  const images = [
+  const [selectedImage, setSelectedImage] = useState<
+    React.ImgHTMLAttributes<HTMLImageElement> | undefined
+  >(undefined);
+  const [dialogOpen, setDialogOpen] = useState(false);
+
+  const images: React.ImgHTMLAttributes<HTMLImageElement>[] = [
     {
       src: "/img/img2.jpg",
       alt: "",
@@ -31,17 +47,42 @@ export default function Images() {
     },
   ];
 
+  function handleCarouselItemClick(
+    image: React.ImgHTMLAttributes<HTMLImageElement>
+  ) {
+    setDialogOpen(true);
+    setSelectedImage(image);
+  }
+
   return (
-    <Carousel className="mx-24">
-      <CarouselContent>
-        {images.map((image) => (
-          <CarouselItem className="md:basis-auto flex justify-center">
-            <CarouselImage {...image} />
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
+    <>
+      <Carousel className="mx-24">
+        <CarouselContent>
+          {images.map((image) => (
+            <CarouselItem
+              className="md:basis-auto flex justify-center"
+              onClick={() => handleCarouselItemClick(image)}
+            >
+              <CarouselImage {...image} />
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+      <Dialog open={dialogOpen}>
+        <DialogContent className="flex flex-col justify-center">
+          <DialogHeader>
+            <DialogTitle>John's Tack Repair Service</DialogTitle>
+            <DialogDescription>{selectedImage?.alt}</DialogDescription>
+          </DialogHeader>
+          <ImageProxy
+            options={{ format: "webp", height: 510 }}
+            {...selectedImage}
+            className="h-96 lg:h-[500px] w-auto"
+          />
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
